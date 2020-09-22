322556c6418f964dffe7e9f32ad5cb6967c2fa2c
Caue Borella <caueborella@gmail.com>
2020-09-22 10:04:04 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -474,8 +474,8 @@ function App() {
   return (
     <div className="App">
       <header>ZeroHeight Docs Generator</header>
+      <strong> NOTE:  </strong> When in doubt consult the current <a href="https://atomix.hexagonmining.com/"> Atomix documentation </a> for reference.
       <hr />
-    <strong> NOTE:  </strong> When in doubt consult the current <a href="https://atomix.hexagonmining.com/"> Atomix documentation </a> for reference.
       <h2>About Tab</h2>
       <h3>What is my component?</h3>
       <p>
